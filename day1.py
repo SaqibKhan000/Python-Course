2d61b75ce0799b9080ce68efe6bebd393c655cd4
M Saqib Khan <saqibkhannn543@gmail.com>
2026-01-30 17:54:15 +0500

    day1 of python

---
+++ day1.py
@@ -0,0 +1,14 @@
+# This is my first program in Python
+print("Hello World")
+
+
+# Calculation in Python
+print(1 + 1)
+print(2 - 1)
+print(3 * 2)
+print(4 / 2)
+
+
+# Variable with string
+name = 'M Saqib Khan'
+print("Hello", name, sep=" - ")
