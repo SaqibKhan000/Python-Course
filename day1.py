2db53bd51849f9aa9eb993d5b8705ef672e3624a
M Saqib Khan <saqibkhannn543@gmail.com>
2026-01-31 22:07:23 +0500

    Variables in python

--- day1.py
+++ day1.py
@@ -8,7 +8,7 @@ print(2 - 1)
 print(3 * 2)
 print(4 / 2)
 
-
+ 
 # Variable with string
 name = 'M Saqib Khan'
 print("Hello", name, sep=" - ")
---
+++ day2.py
@@ -0,0 +1,32 @@
+
+# Variables in Python
+a = 1
+print(a)
+
+name = 'M Saqib Kha'
+print(name)
+
+num = 50
+print(num + 10)
+
+
+# Multiple variables initialization:
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+
+# Variable casing in Python:
+MyName = "M Saqib Khan" # pascal case
+myName = "M Saqib Khan" # camel case
+myname = "M Saqib Khan" # flat case
+my_name = "M Saqib Khan" # snake case
+
+
+
+# Rules for variables in Python:
+# 1) Variables must start from a letter like (a, b, c....z) or underscore (_).
+# 2) Variable names can contain letters, numbers, underscore.
+# 3) Here varibales are case-sensitive.
+# 4) Variable names can't be reserved words.
+
+
