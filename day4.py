6e1b07fa7bd81a02f94a68a66265b63a01f8616a
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-02 22:06:58 +0500

    Python first assignment

---
+++ day4.py
@@ -0,0 +1,23 @@
+
+
+
+
+################# ASSIGNMENT NO 01 #################
+
+# Question: 1
+sentence = '''"Quotes" and 'Single Quotes' can be tricky'''
+# OR
+sentence = '"Quotes" and \'Single Quotes\' can be tricky'
+print(sentence)
+
+
+
+
+
+
+
+# Question: 2
+name = "M Saqib Khan"
+age = 18
+city = "Peshawar"
+print(f"My name is {name}, I am from {city} and I am {age} years old")# // 'f' mean formated string
