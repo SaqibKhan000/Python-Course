f73ac626c7f9770021b17ea7fad1a046c21a4f64
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-05 20:52:13 +0500

    Python Assignment No 2

--- day6.py
+++ day6.py
@@ -19,10 +19,3 @@ print(f"Total is {int(num1) + int(num2)}")
 
 
 
-# Student Marks:
-print("Enter your marks below:")
-phy = input("Enter Physics marks: ")
-che = input("Enter Chemistry marks: ")
-bio = input("Enter Biology marks: ")
-sum = int(phy) + int(che) + int(bio)
-print(f"Your percentage is {sum / 300 * 100}%")
---
+++ day7.py
@@ -0,0 +1,31 @@
+
+
+# Assignment No: 1
+# Student Marks:
+print("Enter your marks below:")
+phy = input("Enter Physics marks: ")
+che = input("Enter Chemistry marks: ")
+bio = input("Enter Biology marks: ")
+sum = int(phy) + int(che) + int(bio)
+print(f"Your percentage is {sum / 300 * 100}%")
+
+
+
+
+
+
+
+
+
+
+# Assignment No: 2
+# User Details:
+userDetails = {}
+userDetails["name"] = input("Enter name: ")
+userDetails["age"] = int(input("Enter age: "))
+userDetails["height"] = float(input("Enter height: "))
+print(userDetails)
+
+
+
+
