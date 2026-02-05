904c7783bde81ad2590eb4910b9cb78b31883bdb
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-05 14:15:08 +0500

    Input function in python

---
+++ day6.py
@@ -0,0 +1,28 @@
+
+
+
+###    INPUT FUNCTION IN PYTHON:
+
+
+# Printing user name:
+name = input("Enter you name: ")
+print(f"Your name is {name}")
+
+
+
+
+# Adding two numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+print(f"Total is {int(num1) + int(num2)}")
+
+
+
+
+# Student Marks:
+print("Enter your marks below:")
+phy = input("Enter Physics marks: ")
+che = input("Enter Chemistry marks: ")
+bio = input("Enter Biology marks: ")
+sum = int(phy) + int(che) + int(bio)
+print(f"Your percentage is {sum / 300 * 100}%")
