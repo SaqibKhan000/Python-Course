3a47369638bffbb5cc0789615d837391759c109b
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-06 20:52:03 +0500

    Operators in Python

---
+++ day8.py
@@ -0,0 +1,58 @@
+# We Have Total Seven (7) Operators In Python:
+
+
+# 1) ARITHMATIC OPERATOR:
+num1 = 10
+num2 = 5
+print(num1 + num2) # Addition Operator
+print(num1 - num2) # Subtraction Operator
+print(num1 * num2) # Multiplication Operator
+print(num1 / num2) # Division Operator
+print(num1 // num2) # Floor Division Operator
+print(num1 % num2) # Modulus Operator
+print(num1 ** num2) # Exponentiation Operator
+
+
+
+
+# 2) ASSIGNMENT OPERATOR:
+num3 = 8 # Assignment Operator (=)
+num3 += 1 # Assignment Operator (+=) 
+num3 -= 1 # Assignment Operator (-=) 
+num3 *= 1 # Assignment Operator (*=) 
+num3 /= 1 # Assignment Operator (/=) 
+num3 %= 1 # Assignment Operator (%=) 
+num3 //= 1 # Assignment Operator (//=) 
+
+
+
+
+
+# 3) COMPARISON OR RELATIONAL OPERATOR:
+num4 = 8
+num5 = 5
+print(num4 == num5) # equal to 
+print(num4 != num5) # not equal to
+print(num4 > num5) # greater than
+print(num4 < num5) # less than
+print(num4 >= num5) # greater than or equal to
+print(num4 <= num5) # less than or equal to
+
+
+
+
+
+# 4) LOGICAL OPERATOR:
+num6 = 15
+num7 = 10
+print((num6 > num7) and (num7 < num6)) # and operator
+print((num6 > num7) or (num7 > num6))  # or operator
+print(not(num6 > num7)) # not operator
+
+
+
+
+
+
+
+
