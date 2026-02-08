353c5c04b7adf7497b93861f6b99e316e08b4587
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-08 22:17:35 +0500

    Conditional Statement In Pytho

---
+++ day9.py
@@ -0,0 +1,25 @@
+
+
+# IF STATEMENT IN PYTHON:
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You can drive")
+
+
+
+
+
+
+# IF-ELSE STATEMENT IN PYTHON:
+temp = 28
+if temp <= 18:
+    print("It's a cool day.")
+else:
+    print("It's a hot day.")    
+
+
+
+
+
+
+# IF ELIF ELSE STATEMENT IN PYTHON:   
