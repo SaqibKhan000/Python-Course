d49637a75c33d6ff3c890f7049049afc2ac535a2
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-12 18:44:10 +0500

    Function in Python

---
+++ day11.py
@@ -0,0 +1,23 @@
+# Simple Function Example In Python
+def myName():
+    print("I am M Saqib Khan. The future Millionaire.")
+myName()
+
+
+
+
+# Function With Params & Arguments
+def addNums(num1, num2):
+    print(num1 + num2)
+addNums(10, 20)
+
+
+
+
+# Function With Return Keyword
+def sayHello():
+    return 'Hello'
+result = sayHello()
+print(result)
+
+
