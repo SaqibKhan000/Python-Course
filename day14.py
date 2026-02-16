ed3c7c71df24f2a83699c7f5dd1077e5b4ff6a68
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-16 19:38:54 +0500

    String Formating and Operators in Python

---
+++ day14.py
@@ -0,0 +1,30 @@
+
+# STRING FORMATING AND STRING OPERATORS:
+
+
+print('''It's me M Saqib Khan''') # If you want to use single quote inside single quote or double quote inside double quote you can use three times quotes for this case.
+
+
+# 1) Old Style Formating (% Operator):
+my_name = "M Saqib Khan"
+my_age = 18
+print("My name is %s and I'm %d" % (my_name, my_age))
+# %s and %d are placeholders, %s represents string while %d represents digit(numeric value).
+
+
+
+# 2) str.format() method
+name1 = "Ali"
+age1 = 20
+print("My name is {} and I'm {}". format(name1, age1))
+
+# We can also reference variables by index or keywords
+print("My name is {0} and I'm {1}".format(name1, age1))
+print("My name is {name1} and I'm {age1}".format(name1 = 'Saqib', age1 = 19))
+
+
+
+# 3) f-string (Very usable and simple string formating technique):
+name2 = "Ahmad"
+age2 = 22
+print(f"My name is {name2} and I'm {age2}")
