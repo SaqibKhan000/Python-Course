50676fd96a94ea71e5bcd4150c25bc74e7431beb
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-21 19:37:25 +0500

    Loop Control Statements

--- day17.py
+++ day17.py
@@ -27,7 +27,7 @@ else:
     print("For loop ended")
 
 # range():
-#        This method is specially for 'for' loop. range(start, stop, step)
+#        This function is specially for 'for' loop. range(start, stop, step)
 # start mean start index.
 # stop mean end index, mean where the loop will exit and this index is exclusive.
 # step defines the step of the iterations. Step is by default (1).
@@ -37,8 +37,62 @@ for i in range(1, 5):
 
 for j in range(5):
     print(j) # 1  2  3  4 
-# When we give just one value to range() method, that value only for stop
+# When we give just one value to range() function, that value only for stop
 
 
 for k in range(1, 10, 2):
     print(k) # 1  3  5  7  9 . Because of step 2.
+else:
+    print("For loop ended")
+
+
+
+
+
+# Loop Control Statements:
+
+# Pass Statement:
+number = 1
+while number <= 5:
+    if(number == 3):
+       pass
+    print(number)
+    number += 1
+# Pass Statmement doesn't any work, but it is used to prevent ourselves from syntax error. For example as you see in the above example I write a condition and inside I write the pass statement so if I don't write this I will take a syntax error. So if you want to leave a particular area for a future code but directly leaving the area will cause syntax error that's pass statement is use for this problem.
+
+
+# Break Statement:
+for num1 in range(1, 5):
+    if(num1 == 4):
+        print("Loop ended")
+        break
+    print(num1)
+
+
+
+
+# Continue Statement:
+num2 = 1
+while num2 < 10:
+    if(num2 == 5):
+        num2 += 1
+        continue
+    print(num2)
+    num2 += 1
+
+
+
+
+
+
+
+
+# For practice purpose:
+while True:
+    user_input = input("Enter exit to stop the loop:")
+    if(user_input == "exit"):
+        print("Congrates! loop was exited.")
+        break
+    else:
+        print(F"{user_input} is not a proper command")
+   
