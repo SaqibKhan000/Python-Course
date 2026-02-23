0168b8c425bae3d29ef10a4a667800522949a365
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-23 17:54:20 +0500

    Assignment: 5

---
+++ day19.py
@@ -0,0 +1,26 @@
+
+########### ASSIGNMENT: 5 ############
+
+# 1) Use while loop to print the output in the same line
+# i = 0
+# while(i < 5):
+#     print(i, end=" ")
+#     i += 1
+
+
+
+
+#2.a: Triangle Pattern 
+# for j in range(1, 11):
+#     print("*" * j)
+
+
+#2.b: Inverted Triangle Pattern 
+# i = 10
+# while i >= 1:
+#     print("*" * i)
+#     i -= 1
+
+
+
+
