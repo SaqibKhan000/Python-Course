1a2cf47afa2c49e80e5d14541faac68ecdefbfc9
M Saqib Khan <saqibkhannn543@gmail.com>
2026-02-24 19:14:37 +0500

    Remaining tasks added of Assignment 5

--- day19.py
+++ day19.py
@@ -10,12 +10,12 @@
 
 
 
-#2.a: Triangle Pattern 
+#2) a: Triangle Pattern 
 # for j in range(1, 11):
 #     print("*" * j)
 
 
-#2.b: Inverted Triangle Pattern 
+# b: Inverted Triangle Pattern 
 # i = 10
 # while i >= 1:
 #     print("*" * i)
@@ -24,3 +24,56 @@
 
 
 
+
+
+# 3) Find factorial of any number
+# def factorial(number):
+#          result = 1
+#          while number > 0:
+#                  result *= number
+#                  number -= 1
+#          return result
+# print(factorial(5))
+
+
+
+
+
+# 4) Count the number of vowels in a string
+# vowels = "aeiou"
+# def vowelsCount(word):
+#     count = 0
+#     for character in word:
+#         if character.lower() in vowels:
+#             count += 1
+#     return count
+
+# print(vowelsCount("M Saqib Khan"))
+
+
+
+
+
+
+# 5) Longest word in a string
+# sentence = 'I am M Saqib Khan'
+# longest_word = ""
+# words_list = sentence.split()
+# for word in words_list:
+#     if len(word) > len(longest_word):
+#         longest_word = word
+# print(longest_word)
+
+
+
+
+
+
+# 6) do-while loop in python 
+# while True:
+#     num = int(input("Enter a number which is greater than ten(10): "))
+#     if num > 10:
+#         print("You entered correct number ", {num})
+#         break
+#     else:
+#         print(f"{num} is not greater than ten(10). Try again")
