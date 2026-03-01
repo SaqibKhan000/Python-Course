797b10454fe7ee418857452cae99243093c271ec
M Saqib Khan <saqibkhannn543@gmail.com>
2026-03-01 16:35:35 +0500

    All Tuple Topics Added

--- day21.py
+++ day21.py
@@ -34,7 +34,7 @@ print(tuple4[-1]) # blue
 
 
 # Tuple Slicing
-tuple5 = ("red", "green", "blue", "orange", "puple")
+tuple5 = ("red", "green", "blue", "orange", "purple")
 print(tuple5[1:2]) # ('green',)
 print(tuple5[0::2]) # ('red', 'blue', 'puple')
 print(tuple5[::-1]) # ('puple', 'orange', 'blue', 'green', 'red')
@@ -42,7 +42,7 @@ print(tuple5[::-1]) # ('puple', 'orange', 'blue', 'green', 'red')
 
 # Tuple Operations
 # 1) Concatenation:
-tuple6 = ('puple', 'orange', 'blue')
+tuple6 = ('purple', 'orange', 'blue')
 tuple7 = ('green', 'red')
 print(tuple6 + tuple7)
 
@@ -57,7 +57,7 @@ print(4 not in tuple9) # False
 
 
 # Iteration Over Tuple:
-tuple10 = ('puple', 'orange', 'blue')
+tuple10 = ('purple', 'orange', 'blue')
 for item in tuple10:
     print(item)
 
@@ -66,3 +66,116 @@ index = 0
 while index < len(tuple10):
     print(tuple10[index])
     index += 1
+
+
+
+#----------------------------------------
+
+
+
+
+# Tuple Methods:
+# Tuple has very less methods because it's immutable
+
+# 1) count()
+tuple11 = ('purple', 'orange', 'blue')
+print(tuple11.count('purple')) # 1
+
+
+# 2) index
+tuple12 = ('purple', 'orange', 'blue')
+print(tuple12.index('blue')) # 2
+
+
+
+
+
+
+#----------------------------------------------
+
+
+
+
+
+
+# Tuple Functions
+
+# 1) len()
+tuple13 = ('purple', 'orange', 'blue')
+print(len(tuple13)) # 3
+
+
+# 2) sum()
+tuple14 = (1, 2, 3, 4)
+print(sum(tuple14)) # 4
+
+
+# 3) min()
+print(min(tuple14)) # 1
+ 
+
+# 4) max()
+print(max(tuple14)) # 4
+
+
+# 5) sorted()
+print(sorted(tuple14)) # It will give us list not tuple
+# Solution is:
+list = sorted(tuple14)
+print(tuple(list)) # (1, 2, 3, 4)
+
+
+
+
+
+
+#-----------------------------------------------
+
+
+
+
+
+# Packing And Unpacking Tuples:
+
+# PACKING: It is the process of putting multiple values into a sinple tuple.
+# UNPACKING: It is extracting the values from a tuple into separate variables.
+
+a = "Saqib"
+b = 18
+c = "Content Creator"
+tuple15 = a, b, c
+print(tuple15) # Packing Tuple: ('Saqib', 18, 'Content Creator')
+
+
+name, age, profession = tuple15 # Unpacking Tuple
+print("Name is ", name)
+print("Age is ", age)
+print("Profession is ", profession)
+
+
+
+
+
+#--------------------------------------------------
+
+
+
+
+
+# Tuple is Immutable but we can modify it indirectly:
+tuple16 = (10, 20, 30)
+tuple16[0] = "ten" 
+print(tuple16) # Will Cause Error❌
+
+# Solution Is:
+list2 = list(tuple16)
+list2[0] = "ten"
+tuple17 = tuple(list2)
+print(tuple17)
+
+
+
+
+
+
+
